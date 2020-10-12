08ec2f64628932e796748ef09af9f064299c29de
Iso <kagiso.k.mashigo@gmial.com>
2020-10-12 18:02:13 +0000

    1_queries complete

---
+++ 1_queries/1_students_names_alphabetical_single_cohort.sql
@@ -0,0 +1,10 @@
+SELECT id, name 
+FROM students 
+WHERE cohort_id = 1
+ORDER BY name;
+
+
+-- Your original answer
+-- SELECT id, name
+-- FROM students
+-- ORDER BY cohort_id;
---
+++ 1_queries/2_total_students_1st_3_cohorts.sql
@@ -0,0 +1,11 @@
+SELECT count(id)
+FROM students 
+WHERE cohort_id IN (1,2,3);
+
+
+
+-- Your answer: 
+-- SELECT COUNT(name) 
+-- FROM students 
+-- WHERE cohort_id LIKE (1, 2, 3)
+-- ORDER BY name;
